a47de3af5caa45e762bebcf95a6fa5de11829f0c
AlexPeterson99 <AlexJoseph.Peterson@CalBaptist.edu>
2020-02-05 14:43:32 -0800

    Take that

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django! From Alex! TRUMP 2020!")
+    return HttpResponse("Hello, Django! From Alex! Get Trumped and try again :(")
