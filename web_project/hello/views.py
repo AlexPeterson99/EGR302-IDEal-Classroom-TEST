5858fd595d43b7ebb674e36412c7091efa827b17
Sebastian Prado <Sebastian.Prado@calbaptist.edu>
2020-02-07 10:14:10 -0800

    test

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django! From Alex! Get Trumped and try again :(")
+    return HttpResponse("Hello, Django! From Alex! Get Trumped and try ")
