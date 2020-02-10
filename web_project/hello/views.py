474400b4650173147a782a6dcc60fc141c9d9ec6
CBUmikes930 <Micahwilliam.steinbock@calbaptist.edu>
2020-02-10 14:38:02 -0800

    Google Cloud Test

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django! From Alex! Get Trumped and try ")
+    return HttpResponse("Hello, Django! This is a test to make sure we can update Google Cloud")
