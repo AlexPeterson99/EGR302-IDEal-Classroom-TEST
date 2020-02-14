87b0400e29b5911a60952011f62dc4a22d3921c7
CBUmikes930 <Micahwilliam.steinbock@calbaptist.edu>
2020-02-13 22:43:02 -0800

    Another update test

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome, Django! This is a test to make sure we can update Google Cloud. Am i updating???")
+    return HttpResponse("Welcome, Django! This is a test to make sure we can update Google Cloud. Yep, its updating")
