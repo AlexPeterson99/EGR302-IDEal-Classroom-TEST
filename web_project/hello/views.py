4ac366db3c8d46c062b083524e4866d4eb46ba7f
CBUmikes930 <Micahwilliam.steinbock@calbaptist.edu>
2020-02-14 09:59:55 -0800

    Response to abanoub's first push

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome, Django! This is a test to make sure we can update Google Cloud. Abanoub is Good!!!!")
+    return HttpResponse("Welcome, Django! This is a test to make sure we can update Google Cloud. Worked here!s")
