472412f15d66ea311c652891411b2786d5f97367
AustenCombs <austencombs@calbaptist.edu>
2020-02-21 01:42:37 -0800

    Webpage functionality Test

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.db import connection
+from .models import TempUsers
+from .models import Course
+from .models import Assignment
+from .models import Submission
 
-# User home page - Edited by Austen Combs on Feb 17, 2020
+# User home page - Edited by Austen Combs on Feb 20, 2020
 def home(request):
-    return render(request, "home.html")
+    users = TempUsers.objects.all()
+    courses = Course.objects.all()
+    submits = Submission.objects.all()
+    return render(request, "home.html", {'users': users, 'courses': courses, 'submits': submits})
 
 # A test page to test if the database integration is working properly - Added by Micah Steinbock on Feb 16, 2020
 def dbtest(request):
@@ -25,3 +32,14 @@ def register(request):
 # User account page - Added by Austen Combs on Feb 17, 2020
 def account(request):
     return render(request, "account.html")
+
+# User course page - Added by Austen Combs on Feb 20, 2020
+def course(request):
+    courses = Course.objects.all()
+    assignments = Assignment.objects.all()
+    return render(request, "course.html", {'courses':courses, 'assignments': assignments})
+
+# User assignments page - Added by Austen Combs on Feb 20, 2020
+def assignment(request):
+    assignments = Assignment.objects.all()
+    return render(request, "assignment.html", {'assignments': assignments})
