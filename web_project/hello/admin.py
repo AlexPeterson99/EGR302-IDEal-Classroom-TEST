d8273eea93bc7916f1e786c30c624c602ecfd723
CBUmikes930 <Micahwilliam.steinbock@calbaptist.edu>
2020-02-21 13:24:23 -0800

    With Hello App gone

--- web_project/hello/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Roster, Course, Assignment, Submission, TempUsers
-
-admin.site.register(Roster)
-admin.site.register(Course)
-admin.site.register(Assignment)
-admin.site.register(Submission)
-admin.site.register(TempUsers)
--- web_project/ideal_classroom/admin.py
+++ web_project/ideal_classroom/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Roster, Course, Assignment, Submission, TempUsers
 
-# Register your models here.
+admin.site.register(Roster)
+admin.site.register(Course)
+admin.site.register(Assignment)
+admin.site.register(Submission)
+admin.site.register(TempUsers)
