cd7b1e8afd80f372d0bdede9ac6d4854904097cd
AlexPeterson99 <AlexJoseph.Peterson@CalBaptist.edu>
2020-03-19 15:52:24 -0700

    test_runner added

---
+++ web_project/test_runner.py
@@ -0,0 +1,25 @@
+# test_runner.py
+#
+# Created By: Alex Peterson     AlexJoseph.Peterson@CalBaptist.edu
+# Last Edited: March 19, 2020
+#
+# Description:
+#       test_runner.py manages processes involved when a student selects to test their code.
+#       Uses utils directory to handle cloning and testing processes.
+#
+# Dependencies:
+# 
+
+#imports
+import compile
+import clone
+
+# Get the information of the user (GitHub ID, Name, Assignment, Course)
+
+
+
+#Pass through information to run tests
+def test_runner(github_id, student_name, assignment, course):
+    clone.cloneGit()   #params need to be fixed. Should not run correctly. 
+    
+    pass
